2a0544af21c4fb885d366b94bc87ee265e52a562
Hye10 <dhye410@daum.net>
2025-08-13 16:21:37 +0900

    [feature] 버튼 컴포넌트 생성 - size, color, disabled에 따라 각 각 다른 버튼 - tailwindcss의 JIT 방식의 빌드로 인해 임시적으로 tailwind.config.ts파일 내에 safelist에 사용하는 classname 정의 (추후 불필요한 class는 제거 예정)

---
+++ src/ui/Button.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { cva } from "class-variance-authority";
+
+type ButtonProps = {
+  size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary";
+  children: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+};
+
+const buttonVariants = cva(["rounded-[20px] px-8 py-4"], {
+  variants: {
+    color: {
+      primary: ["bg-primary"],
+      secondary: ["bg-secondary"],
+      inactivePrimary: ["bg-disabled-primary"],
+      inactiveSecondary: ["bg-disabled-black"],
+    },
+    size: {
+      large: ["text-xl", "w-[280px]"],
+      medium: ["text-base", "w-[200px]"],
+      small: ["text-base", "w-[140px]"],
+    },
+    defaultVariants: {
+      color: "primary",
+      size: "medium",
+    },
+  },
+});
+
+const Button = (props: ButtonProps) => {
+  const { size = "medium", color = "primary", children, onClick, disabled = false } = props;
+
+  const colorVariant = () => {
+    if (disabled) return color === "primary" ? "inactivePrimary" : "inactiveSecondary";
+    return color;
+  };
+
+  return (
+    <button onClick={onClick} className={buttonVariants({ size, color: colorVariant() })} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+export default Button;

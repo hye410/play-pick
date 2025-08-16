9c924df47a31d41fa6a668753d969cf3cd8261b0
Hye10 <dhye410@daum.net>
2025-08-16 22:04:07 +0900

    [feature] 데스크탑 최대값 1440px로 적용

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,10 @@
 import Header from "@/features/layout/Header";
 
-/**MainHeight : 메인 레이아웃 body의 height 값
- * 100dvh(뷰 height) - 2.5rem(body 아래 위 패딩 1.25rem*2) - 2rem (header margin bottom)
- */
-const MainHeight = "100dvh-4.5rem-60px";
-
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-[calc(100dvh-2.5rem)] flex-col bg-green-300">
+    <div className="flex h-[calc(100dvh-2.5rem)] flex-col">
       <Header />
-      <main className="mx-auto w-full flex-1">{children}</main>
+      <main className="mx-auto w-full max-w-[1440px] flex-1">{children}</main>
     </div>
   );
 };

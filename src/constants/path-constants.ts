1f8295664f40c51b6175e8ec5634db9995c3b78e
Hye10 <dhye410@daum.net>
2025-08-16 22:03:10 +0900

    [chore] path 상수화

--- src/constants/path-constants.ts
+++ src/constants/path-constants.ts
@@ -1,4 +1,9 @@
-const BASE_URL = process.env.TMDB_API_URL;
+/** common url  */
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const SUPABASE_IMAGE_URL = process.env.NEXT_PUBLIC_SUPABASE_IMAGE_URL;
+export const TMDB_BASE_URL = process.env.TMDB_API_URL;
+export const TMDB_IMAGE_URL = process.env.NEXT_PUBLIC_TMDB_IMGE_URL;
+
 //**Home > Today's Pick get api */
 export const GET_TODAY_PICK = (contentsType: "movie" | "tv") =>
-  `${BASE_URL}/trending/${contentsType}/day?language=ko-KR`;
+  `${TMDB_BASE_URL}/trending/${contentsType}/day?language=ko-KR`;

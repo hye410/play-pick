5981f63ff73663d88256b9ad808c687373f40416
Hye10 <dhye410@daum.net>
2025-09-04 21:48:31 +0900

    [refactor] use-survey-hook으로 business logic 분리

---
+++ src/features/survey/hook/use-survey-hook.ts
@@ -0,0 +1,90 @@
+"use client";
+
+import { useSurveyAnswersStore } from "@/store/use-survey-answers-store";
+import type { Option, Question } from "@/types/survey-types";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getQuestionsByKey, getSurveyResult } from "@/features/survey/api/services";
+import { ALERT_TYPE } from "@/constants/alert-constants";
+
+const { ERROR } = ALERT_TYPE;
+
+const useSurveyHook = (initialQuestion: Question[]) => {
+  const [questions, setQuestions] = useState<Question[]>(initialQuestion);
+  const { answers, addToAnswers, currentQuestionIndex, setCurrentQuestionIndex } = useSurveyAnswersStore();
+  const currentQuestion = questions[currentQuestionIndex] ?? questions[0];
+  const currentOptions = currentQuestion.options;
+  const isFirstQuestion = currentQuestionIndex <= 0;
+  const isLastQuestion = currentQuestionIndex + 1 >= questions.length;
+  const currentKey = currentQuestion.tmdb_key;
+  const route = useRouter();
+
+  const getRestOfQuestions = async (preferredType: string) => {
+    try {
+      const restQuestions = await getQuestionsByKey(preferredType);
+      setQuestions([...initialQuestion, ...restQuestions]);
+    } catch (error) {
+      alert({
+        type: ERROR,
+        message: error as string,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const selectedType = answers["type"] as string;
+    getRestOfQuestions(selectedType);
+  }, [answers["type"]]);
+
+  const moveToResult = async () => {
+    try {
+      await getSurveyResult(answers, questions);
+      route.replace("/result");
+    } catch (error) {
+      alert({
+        type: ERROR,
+        message: error as string,
+      });
+    }
+  };
+
+  const moveToNext = () => {
+    if (!isLastQuestion) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      moveToResult();
+    }
+  };
+  const moveToPrev = () => {
+    if (!isFirstQuestion) setCurrentQuestionIndex(currentQuestionIndex - 1);
+  };
+
+  const getOptionValue = (value: Option["value"]) => {
+    if (currentQuestion.is_multiple_choice) {
+      const accumulatedAnswers: string[] = Array.isArray(answers[currentKey]) ? answers[currentKey] : [];
+      let newAnswer;
+      if (accumulatedAnswers.includes(value)) {
+        newAnswer = accumulatedAnswers.filter((old) => old !== value);
+      } else {
+        newAnswer = [...accumulatedAnswers, value];
+      }
+      addToAnswers(currentKey, newAnswer);
+    } else {
+      addToAnswers(currentKey, value);
+    }
+  };
+
+  return {
+    currentQuestion,
+    currentOptions,
+    getOptionValue,
+    currentKey,
+    isFirstQuestion,
+    isLastQuestion,
+    moveToPrev,
+    moveToNext,
+    answers,
+  };
+};
+
+export default useSurveyHook;
--- src/features/survey/survey.tsx
+++ src/features/survey/survey.tsx
@@ -1,81 +1,28 @@
 "use client";
 import Button from "@/components/Button";
 import Options from "@/features/survey/options";
-import { useSurveyAnswersStore } from "@/store/use-survey-answers-store";
-import type { Option, Question } from "@/types/survey-types";
+import type { Question } from "@/types/survey-types";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
-import { getQuestionsByKey, getSurveyResult } from "./api/services";
-import { ALERT_TYPE } from "@/constants/alert-constants";
-import { alert } from "@/utils/alert";
+import useSurveyHook from "@/features/survey/hook/use-survey-hook";
 
 type SurveyProps = {
   initialQuestions: Question[];
 };
 
 const APPLY_GRID_BOUNDARY = 12;
-const { ERROR } = ALERT_TYPE;
 
 const Survey = ({ initialQuestions: initialQuestion }: SurveyProps) => {
-  const [questions, setQuestions] = useState(initialQuestion);
-  const { answers, addToAnswers, currentQuestionIndex, setCurrentQuestionIndex } = useSurveyAnswersStore();
-  const currentQuestion = questions[currentQuestionIndex];
-  const currentOptions = currentQuestion.options;
-  const isLastQuestion = currentQuestionIndex + 1 >= questions.length;
-  const isFirstQuestion = currentQuestionIndex <= 0;
-  const currentKey = currentQuestion.tmdb_key;
-
-  const getRestOfQuestions = async (preferredType: string) => {
-    try {
-      const restQuestions = await getQuestionsByKey(preferredType);
-      setQuestions([...initialQuestion, ...restQuestions]);
-    } catch (error) {
-      alert({
-        type: ERROR,
-        message: error as string,
-      });
-    }
-  };
-
-  useEffect(() => {
-    const selectedType = answers["type"] as string;
-    getRestOfQuestions(selectedType);
-  }, [answers["type"]]);
-  console.log("questions=>", questions);
-  console.log("answer=>", answers);
-
-  const moveToResult = async () => {
-    try {
-      await getSurveyResult(answers, questions);
-    } catch (error) {
-      console.log("error=<>", error);
-    }
-  };
-  const moveToNext = () => {
-    if (!isLastQuestion) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      moveToResult();
-    }
-  };
-  const moveToPrev = () => {
-    if (!isFirstQuestion) setCurrentQuestionIndex(currentQuestionIndex - 1);
-  };
-
-  const getOptionValue = (value: Option["value"]) => {
-    if (currentQuestion.is_multiple_choice) {
-      const accumulatedAnswers: string[] = Array.isArray(answers[currentKey]) ? answers[currentKey] : [];
-      let newAnswer;
-      if (accumulatedAnswers.includes(value)) {
-        newAnswer = accumulatedAnswers.filter((old) => old !== value);
-      } else {
-        newAnswer = [...accumulatedAnswers, value];
-      }
-      addToAnswers(currentKey, newAnswer);
-    } else {
-      addToAnswers(currentKey, value);
-    }
-  };
+  const {
+    currentQuestion,
+    currentOptions,
+    getOptionValue,
+    currentKey,
+    isFirstQuestion,
+    isLastQuestion,
+    moveToNext,
+    moveToPrev,
+    answers,
+  } = useSurveyHook(initialQuestion);
 
   return (
     <div className="flex h-full flex-col items-center justify-between">

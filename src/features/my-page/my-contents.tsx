cbc5d13fdaee1b246051522ab2727bda5b87284c
Hye10 <dhye410@daum.net>
2025-10-14 14:42:30 +0900

    [refactor] 마이페이지 mobile 가로모드 반응형 적용

--- src/features/my-page/my-contents.tsx
+++ src/features/my-page/my-contents.tsx
@@ -1,3 +1,4 @@
+import { MY_PAGE_BOX_SIZE } from "@/app/(main)/my-page/page";
 import MyContentsList from "@/features/my-page/my-contents-list";
 import type { User } from "@supabase/supabase-js";
 
@@ -7,7 +8,7 @@ type MyContentsProps = {
 
 const MyContents = async ({ user }: MyContentsProps) => {
   return (
-    <section className="h-full">
+    <section className={`h-full max-h-[calc(100dvh-${MY_PAGE_BOX_SIZE})]`}>
       <MyContentsList userId={user.id} />
     </section>
   );
--- src/features/my-page/my-info.tsx
+++ src/features/my-page/my-info.tsx
@@ -27,7 +27,6 @@ const initialState: InitReturnType = {
   message: null,
 };
 
-const MY_INFO_SIZE = "12.5rem"; // (HEADER_SIZE : 9.5rem) + (TAB_SIZE : 3rem)
 const MyInfo = ({ userEmail }: MyInfoProps) => {
   const [isPending, startTransition] = useTransition();
   const [state, requestUpdatePassword] = useActionState(updatePassword, initialState);
@@ -63,36 +62,37 @@ const MyInfo = ({ userEmail }: MyInfoProps) => {
   };
 
   return (
-    <section className={`mx-auto flex h-full w-[80%] flex-col overflow-y-auto xs:w-2/3 lg:max-w-[600px]`}>
+    <section className={`mx-auto h-full w-[90%] overflow-y-auto xs:w-2/3 lg:max-w-[600px]`}>
       <h3 className="hidden">내 정보 페이지</h3>
-
-      <input
-        defaultValue={userEmail}
-        readOnly
-        className="mb-10 mt-auto w-full rounded-lg bg-secondary px-3 py-4 text-sm text-white sm:text-base"
-      />
-      <form onSubmit={handleSubmit(handleUpdatePassword)} className="mb-5 w-full">
-        <FormInput
-          name="password"
-          control={control}
-          autoFocus={true}
-          placeholder="비밀번호를 입력해 주세요."
-          type="password"
-          inputClassName="text-sm sm:text-base"
+      <div className="flex h-full min-h-fit flex-col items-center justify-center py-10">
+        <input
+          defaultValue={userEmail}
+          readOnly
+          className="mb-10 mt-auto w-full rounded-lg bg-secondary px-3 py-4 text-sm text-white sm:text-base"
         />
-        <FormInput
-          name="confirmPassword"
-          control={control}
-          type="password"
-          placeholder="비밀번호를 확인해 주세요."
-          inputClassName="text-sm sm:text-base"
-        />
-        <p className="mb-8 whitespace-pre-line text-[12px] sm:text-sm">{PASSWORD_CONDITION}</p>
-        <Button type="submit" disabled={isPending}>
-          {isPending ? <LoadingSpinner width="24px" height="24px" /> : "비밀번호 변경"}
-        </Button>
-      </form>
-      <DeleteUserField />
+        <form onSubmit={handleSubmit(handleUpdatePassword)} className="mb-5 w-full">
+          <FormInput
+            name="password"
+            control={control}
+            autoFocus={true}
+            placeholder="비밀번호를 입력해 주세요."
+            type="password"
+            inputClassName="text-sm sm:text-base"
+          />
+          <FormInput
+            name="confirmPassword"
+            control={control}
+            type="password"
+            placeholder="비밀번호를 확인해 주세요."
+            inputClassName="text-sm sm:text-base"
+          />
+          <p className="mb-8 whitespace-pre-line text-[12px] sm:text-sm">{PASSWORD_CONDITION}</p>
+          <Button type="submit" disabled={isPending}>
+            {isPending ? <LoadingSpinner width="24px" height="24px" /> : "비밀번호 변경"}
+          </Button>
+        </form>
+        <DeleteUserField />
+      </div>
     </section>
   );
 };

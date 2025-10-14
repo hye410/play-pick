57ffdfa13e196c475288242b2b8ce18ff183f523
Hye10 <dhye410@daum.net>
2025-10-14 15:38:57 +0900

    [refactor] 메인 타이틀 mt 변경

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,7 +8,7 @@ const Home = async () => {
   return (
     <section className="flex h-full flex-col justify-center">
       <div className="mb-8 sm:mb-16">
-        <h3 className="mb-8 text-center text-3xl font-extrabold">Today&apos;s Pick</h3>
+        <h3 className="my-8 text-center text-3xl font-extrabold">Today&apos;s Pick</h3>
         <ContentsBox contents={contents} />
       </div>
       <div className="mx-auto mb-4 w-[80%] min-w-[260px] sm:w-1/2 lg:w-1/3">

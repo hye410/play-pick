1ed8b88b79d0fef5bcc1f963ff4a4bdc18587f0c
Hye10 <dhye410@daum.net>
2025-10-17 15:09:42 +0900

    [fix] empty-contents height 100%로 수정

--- src/features/my-page/empty-contents.tsx
+++ src/features/my-page/empty-contents.tsx
@@ -1,7 +1,7 @@
 import { LuHeartOff } from "react-icons/lu";
 const EmptyContents = ({ message }: { message: string }) => {
   return (
-    <div className="flex h-[90%] flex-col items-center justify-center text-lg">
+    <div className="flex h-full flex-col items-center justify-center text-lg">
       <LuHeartOff size={"50px"} />
       <span className="mt-4">{message}</span>
     </div>

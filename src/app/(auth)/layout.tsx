72310aeed0a8dab95bdd535a647ff012f99c8e57
Hye10 <dhye410@daum.net>
2025-10-19 16:17:35 +0900

    [fix] alert 창 열릴 시, auth layout에 속하는 페이지들 레이아웃 shifting 발생하는 문제 해결

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main className={`mx-auto h-full max-w-screen-2xl`}>{children}</main>;
+  return (
+    <main className={`h-[calc(100dvh-var(--main-padding-y))] w-full max-w-screen-2xl py-[var(--main-padding-y)]`}>
+      {children}
+    </main>
+  );
 };
 
 export default AuthLayout;
